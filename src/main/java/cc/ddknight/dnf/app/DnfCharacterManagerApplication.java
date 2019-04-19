65a67893e44e8bf7e5f8ec51bb9ff2ab18cd584a
ddknight <cc199026@gmail.com>
2019-04-19 09:02:51 +0800

    project code init by start.spring.io

---
+++ src/main/java/cc/ddknight/dnf/app/DnfCharacterManagerApplication.java
@@ -0,0 +1,13 @@
+package cc.ddknight.dnf.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnfCharacterManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnfCharacterManagerApplication.class, args);
+	}
+
+}
